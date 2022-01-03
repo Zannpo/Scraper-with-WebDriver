d634642e0f9d7f917b948d2fa151e6d748cb9459
Żan <zanpo00@gmail.com>
2022-01-03 13:31:18 +0100

    add inputs

--- src/test/java/page/WordPage.java
+++ src/test/java/page/WordPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 public class WordPage extends BasePage{
     public By linkToCookiesWindow = By.className("abstractButtonAppearance");
     public By linkToAcceptCookies = By.cssSelector(".buttonText");
+    public By linkToInputWord = By.xpath("/html/body/div[2]/div[2]/div[2]/div[2]/div/form/div[1]/input");
     public By linkToSearchingLoop = By.xpath("/html/body/div[2]/div[2]/div[2]/div[2]/div/form/button");
     public By linkToPronounciation = By.xpath("/html/body/div[3]/div[2]/div[2]/div[1]/div/div[1]/div[1]/h1/span[2]/span[1]/span");
     public By linkToGetSentences = By.xpath("/html/body/div[3]/div[2]/div[2]/div[1]/div/div[1]/div[3]/div[3]/a");
--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -23,13 +23,13 @@ public class BaseTest {
         //WebDriverManager.chromedriver().setup();
         System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.navigate().to("https://www.diki.pl/slownik-angielskiego?q=scraping");
-        //driver.navigate().to("https://azure.microsoft.com/pl-pl/services/devops/#overview");
+        driver.navigate().to("https://www.diki.pl");
+        //driver.navigate().to("https://www.diki.pl/slownik-angielskiego?q=scraping");
         driver.manage().window().maximize();
     }
 
     @Test
-    public void demo(){
+    public void checkAWord(){
         WordPage wordPage = new WordPage(driver);
         WebDriverWait wait = new WebDriverWait(driver,10);
         final WebElement linkToCookiesWindow = driver.findElement(By.className("abstractButtonAppearance"));
@@ -38,6 +38,8 @@ public class BaseTest {
             wait.until(ExpectedConditions.elementToBeClickable(linkToCookiesWindow));
             linkToCookiesWindow.findElement(By.cssSelector(".buttonText")).click();
         }
+        driver.findElement(wordPage.linkToInputWord).sendKeys("scraping");
+        driver.findElement(wordPage.linkToSearchingLoop).click();
         driver.findElement(wordPage.linkToPronounciation).click();
         driver.findElement(wordPage.linkToGetSentences).click();
         driver.findElement(wordPage.linkToFirstWord).click();
